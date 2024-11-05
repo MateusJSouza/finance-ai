85d6786f33df91237b224aae54da95e346506084
MateusJSouza <mateusjdev@gmail.com>
2024-11-05 13:31:11 -0300

    chore: add hit hooks setup

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const Home = async () => {
   }
 
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex h-full items-center justify-center">
       <UserButton showName />
     </div>
   );
